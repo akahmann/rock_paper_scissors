3d3ae46cbca08d1b02ac3ac2c0f4c3fc1507fa05
Adam Kahmann <akahmann93@gmail.com>
2018-11-20 16:38:29 -0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
+var dbName = "postgres://xyvynfzkbbhyte:1fa921bcd8c58b9b2c9fdbdb341f48718df4fb4f8d8c97534b643942ad66bb7e@ec2-54-83-8-246.compute-1.amazonaws.com:5432/d5hn9ej1pf6kn"
+
 const app = express();
 const port = process.env.PORT || 5000;
-const dbConnectionString = process.env.DATABASE_URL || "something";
+const dbConnectionString = process.env.DATABASE_URL || dbName;
 
 app.get("/person", getPerson);
 
