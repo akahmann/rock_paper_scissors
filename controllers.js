32495b90f98c4a488b03642a3ca01da7c069d07d
Adam Kahmann <akahmann93@gmail.com>
2018-12-04 00:49:38 -0700

    catch up

--- controllers.js
+++ controllers.js
@@ -1,36 +1,101 @@
 const { Pool } = require("pg");
 
-var dbName = "postgres://xyvynfzkbbhyte:1fa921bcd8c58b9b2c9fdbdb341f48718df4fb4f8d8c97534b643942ad66bb7e@ec2-54-83-8-246.compute-1.amazonaws.com:5432/d5hn9ej1pf6kn"
+//var dbName = "postgres://xyvynfzkbbhyte:1fa921bcd8c58b9b2c9fdbdb341f48718df4fb4f8d8c97534b643942ad66bb7e@ec2-54-83-8-246.compute-1.amazonaws.com:5432/d5hn9ej1pf6kn";
+const dbConnectionString = process.env.DATABASE_URL || "postgres://pbjuser:rosebud@localhost:5432/pbjgame";
 const dbConnectionString = process.env.DATABASE_URL || dbName;
-//const dbConnectionString = process.env.DATABASE_URL || dbName;
-console.log("DB string: " + dbConnectionString);
 
 const pool = new Pool({connectionString: dbConnectionString});
 
+/********************************************************************
+* Add a user account to the database.
+********************************************************************/
 function createUser(req, res) {
    console.log("Creating user");
 
-   console.log("Wanting to create: " + req.body.username);
+   var id = req.params.id;
+   var username = req.params.username;
+   var password = req.params.password;
 
-   var result = {status:"success",
-      entity: {id: 99, username: "MONSTER"}
-   };
+   addUserToDb(id, username, password, function(error, result) {
+      console.log("Back from the database with result: ", result);
+      if (error || result == null) {
+         res.status(500).json({success: false, data: error});
+      }
+      else {
+         res.status(200).json(result);
+      }
+   })
 
    res.json(result);
 }
 
+function addUserToDb(id, username, password, callback) {
+   console.log("Adding to the database: " + username);
+
+   var sql = "INSERT INTO users (id, username, password) "
+           + "VALUES ($1::int, $2::string, $3::string)";
+
+   var params = [user_id, id];
+
+   pool.query(sql, params, function(err, result) {
+      if (err) {
+         console.log("Adding to the database: " + username);
+         console.log(err);
+         callback(err, null);
+      }
+
+      console.log("Found result: " + JSON.stringify(result.rows));
+
+      callback(null, result.rows);
+   });
+}
+
+/********************************************************************
+* Add a friend connection to the database.
+********************************************************************/
 function addFriend(req, res) {
    console.log("Adding friend");
 
-   console.log("Wanting to add as friend: " + req.body.username);
+   var user_id = req.params.user_id;
+   var id = req.params.id;
 
-   var result = {status:"success",
-      entity: {id: 99, username: "MONSTER"}
-   };
+   addFriendToDb(user_id, id, function(error, result) {
+      console.log("Back from the database with result: ", result);
+      if (error || result == null) {
+         res.status(500).json({success: false, data: error});
+      }
+      else {
+         res.status(200).json(result);
+      }
+   })
 
    res.json(result);
 }
 
+function addFriendToDb(user_id, id, callback) {
+   console.log("Adding to the database: " + user_id + " with " + id);
+
+   var sql = "INSERT INTO friends (user_id, friend_id) "
+           + "VALUES ($1::int, $2::int), ($2::int, $1::int)";
+
+   var params = [user_id, id];
+
+   pool.query(sql, params, function(err, result) {
+      if (err) {
+         console.log("Error in query: ");
+         console.log(err);
+         callback(err, null);
+      }
+
+      console.log("Found result: " + JSON.stringify(result.rows));
+
+      callback(null, result.rows);
+   });
+}
+
+/********************************************************************
+* Display all friends of a user from the database.
+********************************************************************/
 function displayFriends(req, res) {
    console.log("Displaying all friends");
 
@@ -50,13 +115,13 @@ function displayFriends(req, res) {
 function getFriendsFromDb(id, callback) {
    console.log("Getting all friends from DB from user with id: " + id);
 
-   var sql = "SELECT u.id, username FROM users u JOIN friends f ON f.friend_id = u.id WHERE f.user_id = $1::int"
+   var sql = "SELECT u.id, username FROM users u JOIN friends f ON f.friend_id = u.id WHERE f.user_id = $1::int";
 
    var params = [id];
 
    pool.query(sql, params, function(err, result) {
       if (err) {
-         console.log("Error in query: ")
+         console.log("Error in query: ");
          console.log(err);
          callback(err, null);
       }
@@ -67,14 +132,13 @@ function getFriendsFromDb(id, callback) {
    });
 }
 
+/********************************************************************
+* Add a friend connection to the database.
+********************************************************************/
 function getUser(req, res) {
    console.log("Getting a user");
 
    var id = req.params.id;
-
-   // var sql = "SELECT id, username FROM users WHERE id = $1::int";
-
-   // var params = [id];
 
    getUserFromDb(id, function(error, result) {
       console.log("Back from the database with result:", result);
@@ -97,7 +161,7 @@ function getUserFromDb(id, callback) {
 
    pool.query(sql, params, function(err, result) {
       if (err) {
-         console.log("Error in query: ")
+         console.log("Error in query: ");
          console.log(err);
          callback(err, null);
       }
--- index.js
+++ index.js
@@ -2,76 +2,16 @@ const express = require("express");
 const port = process.env.PORT || 5000;
 
 const path = require("path");
-
-const controllers = require("./controllers.js")
+const controllers = require("./controllers.js");
 
 express()
    .use(express.json()) // supports JSON encoded bodies
    .use(express.urlencoded({extended: true})) // supports url-encoded bodies
    .use(express.static(path.join(__dirname, "public")))
-   .post("/createUser", controllers.createUser)
-   .post("/addFriend", controllers.addFriend)
+   .post("/createUser/:id/:username/:password", controllers.createUser)
+   .post("/addFriend/:user_id/:id", controllers.addFriend)
    .get("/displayFriends/:id", controllers.displayFriends)
    .get("/getUser/:id", controllers.getUser)
    .listen(port, function() {
       console.log("Server listening on port " + port)
    });
-
-// var app = express();
-
-// const { Pool } = require("pg");
-
-// var dbName = "postgres://xyvynfzkbbhyte:1fa921bcd8c58b9b2c9fdbdb341f48718df4fb4f8d8c97534b643942ad66bb7e@ec2-54-83-8-246.compute-1.amazonaws.com:5432/d5hn9ej1pf6kn"
-// const dbConnectionString = process.env.DATABASE_URL || dbName;
-
-// const pool = new Pool({dbConnectionString: dbConnectionString});
-
-
-// app.set('port', (process.env.PORT || 5000));
-// app.use(express.static(__dirname + '/public'));
-
-
-// app.get('/getUser', function(request, response) {
-//    getUser(request, response);
-// });
-
-
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
-
-
-// function getUser(request, response) {
-//    var id = request.query.id;
-
-//    getUserFromDb(id, function(error, result) {
-
-//       if (error || result == null || result.length != 1) {
-//          response.status(500).json({success: false, data: error});
-//       } else {
-//          var person = result[0];
-//          response.status(200).json(result[0]);
-//       }
-//    });
-// }
-
-// function getUserFromDb(id, callback) {
-//    console.log("Getting user from DB with id: " + id);
-
-//    var sql = "SELECT id, username FROM person WHERE id = $1::int";
-
-//    var params = [id];
-
-//    pool.query(sql, params, function(err, result) {
-//       if (err) {
-//          console.log("Error in query: ")
-//          console.log(err);
-//          callback(err, null);
-//       }
-
-//       console.log("Found result: " + JSON.stringify(result.rows));
-
-//       callback(null, result.rows);
-//    });
-
-// }
