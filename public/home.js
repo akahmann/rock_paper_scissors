11e2dfeba3eba5ef08e84a3181f73b83fc996ebc
Adam Kahmann <akahmann93@gmail.com>
2018-12-13 00:48:27 -0700

    finish project

--- public/home.js
+++ public/home.js
@@ -90,7 +90,7 @@ function playGame(opponent_username, username, game_id, opponent_id, player1_id,
       html += "<span>Press update page to see if you opponent has made their selection</span>"
            + "<br><br><button type=\"button\" onclick=\""
            + "updateGamePage('" + opponent_username + "', '" + username + "', '"
-           + game_id + "', '" + opponent_id + "', '" + player1_id + "', '"
+           + opponent_id + "', '" + game_id + "', '" + player1_id + "', '"
            + player2_id + "', '" + option1 + "', '"
            + option2 + "')\">Update Page</button><br><br>"
 
@@ -102,9 +102,9 @@ function playGame(opponent_username, username, game_id, opponent_id, player1_id,
       html += "<span>Press update page to see if you opponent has made their selection</span>"
            + "<br><br><button type=\"button\" onclick=\""
            + "updateGamePage('" + opponent_username + "', '" + username + "', '"
-           + game_id + "', '" + opponent_id + "', '" + player1_id + "', '"
+           + opponent_id + "', '" + game_id + "', '" + player1_id + "', '"
            + player2_id + "', '" + option1 + "', '"
-           + option2 + "')\">Update Page</button><br><br>"
+           + option2 + "')\">Update Page</button><br><br>";
       console.log("This is option1: " + option1);
       console.log("This is option2: " + option2);
       //Then both users must have selected their options
